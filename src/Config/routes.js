f0010e194cefa01efa29b2cf54c80f4e68f6adef
yatziry ortiz <oyatziry@gmail.com>
2021-04-06 14:20:44 -0700

    adds loggedIn selector

--- src/Config/routes.js
+++ src/Config/routes.js
@@ -5,18 +5,19 @@ import Home from '../Pages/Home';
 import Profile from '../Pages/Profile';
 
 import { useRecoilValue } from 'recoil'
-// import { loggedInState } from "../recoil/selectors";
+import { loggedInState } from "../recoil/selectors";
 
 const Routes = () => {
-  // get the user state with useRecoilValue
-  // const user = useRecoilValue(userState);
-  // change the variable to loggedIn
-  // const loggedIn = useRecoilValue(loggedInState);
+  const loggedIn = useRecoilValue(loggedInState);
 
   return (
     <Switch>
       <Route exact path='/' component={ Home } />
-      <Route exact path='/profile' component={ Profile } />
+      { loggedIn && (
+        <Switch>
+          <Route exact path='/profile' component={ Profile } />
+        </Switch>
+      )}
     </Switch>
   )
 }
