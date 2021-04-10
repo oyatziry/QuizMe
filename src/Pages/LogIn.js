00a0adb7dffeae417a1cfdd4b52e448aeff03a1d
yatziry ortiz <oyatziry@gmail.com>
2021-04-09 22:54:51 -0700

    adds login component

--- src/Pages/LogIn.js
+++ src/Pages/LogIn.js
@@ -2,34 +2,48 @@ import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import AuthModel from '../models/auth';
+import { useRecoilState } from 'recoil';
+import { userState } from '../recoil/atoms';
 
 function LogIn(){
-  // find user by username on db
-  // set user to state using useState and useEffect
-  
+  const [user, setUser] = useRecoilState(userState);
+
+  // function fetchUser(username){
+  //   AuthModel.find(username).then((data) => {
+  //     console.log(data);
+  //   })
+  // }
+
+  function handleSubmit(event){
+    event.preventDefault();
+  }
+
   return (
     <Container>
-      <Form.Group controlId="formBasicUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control 
-          type="text" 
-          placeholder="Username" 
-          // onChange={ (e) => setUsername(e.target.value) }
-          // value={username}
-        />
-      </Form.Group>
+      <Form onSubmit={ handleSubmit }>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Username" 
+            // onChange={ (e) => setUsername(e.target.value) }
+            // value={username}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control 
-          type="password" 
-          placeholder="Password" 
-          // onChange={ (e) => setPassword(e.target.value) }
-          // value={password}
-        />
-      </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control 
+            type="password" 
+            placeholder="Password" 
+            // onChange={ (e) => setPassword(e.target.value) }
+            // value={password}
+          />
+        </Form.Group>
 
-      <input type="submit" value="Submit"></input>
+        <input type="submit" value="Submit"></input>
+      </Form>
     </Container>
   );
 }
