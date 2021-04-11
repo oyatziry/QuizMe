6a7e9bd3959158eb9b6da87d99dff5b17b96e0d4
yatziry ortiz <oyatziry@gmail.com>
2021-04-10 20:56:21 -0700

    displays username in profile page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { useRecoilState } from 'recoil';
+import { userState } from '../recoil/atoms';
+
 function Profile() {
+  const [user] = useRecoilState(userState);
+
   return (
     <div>
       <h1> This is the Profile Page </h1>
+      <h3> Hello, welcome back {user}! </h3>
     </div>
   )
 }
